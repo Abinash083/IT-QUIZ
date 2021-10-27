d016d688d47fade9b03e72252feee3eb644749ab
Abinash083 <avinashniure026@gmail.com>
2021-10-28 04:33:01 +0930

    working model page preperation for fact page

---
+++ ck.js
@@ -0,0 +1,77 @@
+var i =0;
+const myArr= [];
+function loadtxt()
+{ 
+    createnextbutton();
+    getText("checktxt.txt");
+    document.getElementById("5").innerHTML = myArr[i];
+}
+
+    
+
+
+
+
+async function getText(file)
+{
+    
+
+        let A = await fetch(file);
+        let B = await A.text();
+        facts =B;
+        const myArr = facts.split(".");
+        document.getElementById("5").innerHTML = myArr[i];
+        if (i>=myArr.length)
+        {   
+            removenextbutton();
+            document.getElementById("5").innerHTML = "last one"; 
+            let btn = document.createElement("button");
+            btn.innerHTML = "Start from first";
+            btn.onclick = function () 
+            { 
+                moreactions(); 
+    
+            };
+
+            document.body.appendChild(btn);
+
+        }
+                
+}
+function createnextbutton()
+{
+    let bton = document.createElement("button");
+    bton.innerHTML = "Next";
+    bton.id="nextbutton";
+    bton.onclick = function () 
+    {
+        i+=1;
+        getText("checktxt.txt");
+    }
+    document.body.appendChild(bton);
+
+}
+   
+function removenextbutton()
+{
+    var bton = document.getElementById('nextbutton');
+    bton.parentNode.removeChild(bton);
+}
+
+
+
+function moreactions() {
+    i = 0;
+    createnextbutton();
+    getText("checktxt.txt");
+    removenextbutton();
+}
+
+/*
+    fetch('checktxt.txt')
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (data) {
+            return data;
+        });*/
