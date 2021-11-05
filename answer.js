0bd9509f4c9e59d28afa3a85d49b40623e47307a
Abinash083 <avinashniure026@gmail.com>
2021-11-05 16:13:16 +0930

    update the question displaying machenism in quiz page

--- answer.js
+++ answer.js
@@ -9,7 +9,7 @@ class quiz{
 
     }
 }
-const quiz1 = new quiz("What was the first computer that had a colour display?","apple II","imac","apple II","BBC Micro","Amiga 500");
+const a = new quiz("What was the first computer that had a colour display?","apple II","imac","apple II","BBC Micro","Amiga 500");
 
 
 
@@ -17,6 +17,25 @@ function Answer1(qu)
 {
     document.getElementById("answer").innerHTML =qu.answer;
     
+}
+
+function display(running)
+{
+    document.getElementById("question").innerHTML =running.question;
+    document.getElementById("opt1").innerHTML =running.option1;
+    document.getElementById("opt2").innerHTML =running.option2;
+    document.getElementById("opt3").innerHTML =running.option3;
+    document.getElementById("opt4").innerHTML =running.option4;
+
+
+}
+
+function nextqt(running)
+{
+    var num= integer.fromCharCode(running);
+    num+=1;
+    var str =String.fromCharCode(num);
+    display(str);
 }
 
 
